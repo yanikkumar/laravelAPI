1ec5ddde6b7a1afd6b8b8907b42fd68505cb8d69
Yanik Kumar <yanikkumar@cdac.in>
2024-09-02 16:46:31 +0530

    Register with custom request

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM php:8.2-fpm
 
+# Install zip extension
+RUN docker-php-ext-install zip
 RUN docker-php-ext-install pdo pdo_mysql
+
+# Install Composer
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
 
 WORKDIR /app
 COPY . .
---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response($user, status: Response::HTTP_CREATED);
+    }
+}
