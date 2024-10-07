e5b7942b857e0217cbd3ff5104ccb3ea540246b7
Yanik Kumar <yanikkumar@cdac.in>
2024-10-07 14:28:10 +0530

    Created users api CRUD

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,32 +3,53 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
     public function index()
     {
-        //
+        return User::all();
     }
 
     public function store(Request $request)
     {
-        //
+        $user = User::create($request->only(
+            'first_name',
+            'last_name',
+            'email'
+        ) + [
+            'password' => Hash::make('password')
+        ]);
+
+        return response($user, Response::HTTP_CREATED);
     }
 
     public function show(string $id)
     {
-        //
+        return User::find($id);
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        $user = User::find($id);
+
+        $user->update($request->only(
+            'first_name',
+            'last_name',
+            'email'
+        ));
+
+        return response($user, Response::HTTP_ACCEPTED);
     }
 
     public function destroy(string $id)
     {
-        //
+        $user = User::destroy($id);
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
